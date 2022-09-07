55971d2acba65310743669b4e7e25bea6176d8ec
sushil <sushiljyotibansal@gmail.com>
2022-09-07 12:37:19 +0530

    push

--- FA/settings.py
+++ FA/settings.py
@@ -80,14 +80,15 @@ WSGI_APPLICATION = 'FA.wsgi.application'
 # # Database
 # # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'accounts',
-        'USER': 'root',
-        'PASSWORD': 'ansh@2026',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'accounts',
+       'USER': 'accountuser',
+       'PASSWORD': 'ansh@1789',
+       'HOST': 'localhost',
+       'PORT': '',
+  
+   }
 }
 # DATABASES = {
 #     "default": {
