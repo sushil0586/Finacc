eecbfd191b38cee3f0beb2e50745ec9731b7b6ce
sushil <sushiljyotibansal@gmail.com>
2022-09-07 12:38:52 +0530

    push

--- FA/settings.py
+++ FA/settings.py
@@ -79,6 +79,9 @@ WSGI_APPLICATION = 'FA.wsgi.application'
 # }
 # # Database
 # # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+
+
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
