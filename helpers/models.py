289d72bc82a5f8050b33ae6e4d1de435604f5685
sushil <sushiljyotibansal@gmail.com>
2022-09-13 20:50:38 +0530

    push

--- helpers/models.py
+++ helpers/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class TrackingModel(models.Model):
     created_at = models.DateTimeField(auto_now_add = True,null=True,blank=True)
     updated_at = models.DateTimeField(auto_now = True)
-   
+    isActive =   models.IntegerField(default=1)
 
     class Meta:
         abstract = True
