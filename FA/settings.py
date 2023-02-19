bff4fa60b07ff1e4104e607ae0b190347f79c10d
Sushil Bansal <sushilbansal86@gmail.com>
2023-02-19 17:55:10 +0530

    push

--- FA/settings.py
+++ FA/settings.py
@@ -16,10 +16,23 @@ SECRET_KEY = 'django-insecure-(zyb)qx!o_p@$vjqscb=p+)8&-(tj(v*ne_=qc(r@7f(%%a5ey
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['146.190.28.215']
 
 AUTH_USER_MODEL = "Authentication.User"
 # Application definition
+
+    
+          
+            
+    
+
+          
+          
+            
+    
+
+     
+  
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,7 +53,6 @@ INSTALLED_APPS = [
     'drf_excel',
     'import_export',
 ]
-
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -81,20 +93,29 @@ WSGI_APPLICATION = 'FA.wsgi.application'
 # }
 # # Database
 # # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-
-
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
        'NAME': 'accounts',
-       'USER': 'postgres',
+       'USER': 'accountuser',
        'PASSWORD': 'ansh@1789',
        'HOST': 'localhost',
-       'PORT': '5432',
-  
+       'PORT': '',
+
    }
 }
+
+    
+          
+            
+    
+
+          
+          
+            
+    
+
+  
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "mssql",
@@ -125,36 +146,32 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
-    # 'DATE_INPUT_FORMATS': ['%d-%m-%Y'],
-    # 'DATETIME_INPUT_FORMATS': ['%d-%m-%Y'],
-    # 'DATETIME_FORMAT': '%d-%m-%Y',
-    'DATETIME_FORMAT': "%d-%m-%Y",
-    'DATE_FORMAT': "%d-%m-%Y",
+    'DATETIME_FORMAT': '%d-%m-%Y',
+    'DATE_FORMAT': '%d-%m-%Y',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'Authentication.jwt.JwtAuthentication',
     ]
 }
-
-# USE_L10N = False
-
-# DATE_INPUT_FORMATS =["%d-%m-%Y"]
-# DATE_FORMATS =   ["%d -%m-%Y"]
-# DATETIME_FORMAT = ["%d-%m-%Y"]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
-LANGUAGE_CODE = 'en-uk'
+LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
+
+    
+          
+            
+    
+
+          
+    
+    
+  
 USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
