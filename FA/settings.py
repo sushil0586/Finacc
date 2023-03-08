4e9d895769d9662b425d706e28a1abb24b7883a5
sushil0586 <85013199+sushil0586@users.noreply.github.com>
2023-03-08 09:51:28 +0530

    Update settings.py

--- FA/settings.py
+++ FA/settings.py
@@ -168,7 +168,7 @@ USE_L10N = True
     
     
   
-USE_TZ = True
+USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
