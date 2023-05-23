5c8e15d10cc7648560b86bfad989e17d8928c340
Sushil Bansal <sushilbansal86@gmail.com>
2023-05-23 18:07:43 +0530

    payroll push

--- payroll/admin.py
+++ payroll/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from payroll.models import salarycomponent,employeesalary,employee
+from payroll.models import salarycomponent,employeesalary,employee,department,designation
 
 
 # Register your models here.
@@ -8,4 +8,6 @@ from payroll.models import salarycomponent,employeesalary,employee
 admin.site.register(salarycomponent)
 admin.site.register(employee)
 admin.site.register(employeesalary)
+admin.site.register(department)
+admin.site.register(designation)
 
--- payroll/models.py
+++ payroll/models.py
@@ -11,6 +11,33 @@ from entity.models import entity
 from inventory.models import Product
 from django.db.models import Sum 
 import datetime
+from geography.models import country,state,district,city
+
+
+
+
+class department(TrackingModel):
+
+    departmentname = models.CharField(max_length= 200,verbose_name= 'department name')
+    departmentcode = models.CharField(max_length= 200,verbose_name= 'department code')
+    entity = models.ForeignKey(entity,on_delete=models.CASCADE,verbose_name= 'entity',null= True)
+    createdby = models.ForeignKey(to= User, on_delete= models.CASCADE,null=True)
+
+
+    def __str__(self):
+        return f'{self.departmentname}'
+    
+
+class designation(TrackingModel):
+
+    designationname = models.CharField(max_length= 200,verbose_name= 'designation name')
+    designationcode = models.CharField(max_length= 200,verbose_name= 'designation code')
+    entity = models.ForeignKey(entity,on_delete=models.CASCADE,verbose_name= 'entity',null= True)
+    createdby = models.ForeignKey(to= User, on_delete= models.CASCADE,null=True)
+
+
+    def __str__(self):
+        return f'{self.designationname}'
 
 
 
@@ -39,7 +66,20 @@ class salarycomponent(TrackingModel):
 class employee(TrackingModel):
 
     employee = models.OneToOneField(to= User, on_delete= models.CASCADE,primary_key=True)
-
+    employeeid = models.CharField(max_length= 200,verbose_name= 'employee id')
+    dateofjoining = models.DateField(verbose_name='Date Of Joining',default=datetime.date.today)
+    department = models.ForeignKey(department,on_delete=models.CASCADE,verbose_name= 'department',null= True)
+    designation = models.ForeignKey(designation,on_delete=models.CASCADE,verbose_name= 'designation',null= True)
+    reportingmanager = models.ForeignKey("self", on_delete= models.CASCADE,null=True,verbose_name='Reporting Manager',related_name='rmanager')
+    bankname = models.CharField(max_length= 50,verbose_name= 'Bank Name',blank = True,null=True)
+    bankaccountno = models.CharField(max_length= 20,verbose_name= 'Bank Account No',blank = True,null=True)
+    pan = models.CharField(max_length= 20,verbose_name= 'Pan Card details',blank = True,null=True)
+    address1       = models.CharField(max_length=50, null=True,verbose_name='Address Line 1',blank = True)
+    address2       = models.CharField(max_length=50, null=True,verbose_name='Address Line 2',blank = True)
+    country       = models.ForeignKey(country,on_delete=models.CASCADE,null=True)
+    state       = models.ForeignKey(to=state,on_delete=models.CASCADE,null=True)
+    district    = models.ForeignKey(to=district,on_delete=models.CASCADE,null=True)
+    city       = models.ForeignKey(to=city,on_delete=models.CASCADE,null=True)
     entity = models.ForeignKey(entity,on_delete=models.CASCADE,verbose_name= 'entity',null= True)
     createdby = models.ForeignKey(to= User, on_delete= models.CASCADE,null=True,related_name='employeeuser')
 
@@ -51,6 +91,7 @@ class employeesalary(TrackingModel):
     scomponent = models.ForeignKey(to= salarycomponent, on_delete= models.CASCADE,null=True)
     percentageofctc =  models.DecimalField(max_digits=10, decimal_places=2,default=0,verbose_name= 'percentage of ctc')
     salaryvalue=  models.DecimalField(max_digits=10, decimal_places=2,default=0,verbose_name= 'Component value')
+    monthlysalaryvalue=  models.DecimalField(max_digits=10, decimal_places=2,default=0,verbose_name= 'Monthly Component value')
     entity = models.ForeignKey(entity,on_delete=models.CASCADE,verbose_name= 'entity',null= True)
     createdby = models.ForeignKey(to= User, on_delete= models.CASCADE,null=True,related_name='salaryuser1')
 
@@ -73,7 +114,7 @@ class salarytransdetails(TrackingModel):
     salaryamountexpected =  models.DecimalField(max_digits=10, decimal_places=2,default=0,verbose_name= 'Salary amount Expected')
     salaryamountactual =  models.DecimalField(max_digits=10, decimal_places=2,default=0,verbose_name= 'Salary amount Actual')
     arrear =  models.DecimalField(max_digits=10, decimal_places=2,default=0,verbose_name= 'arrear')
-    tottalamount =  models.DecimalField(max_digits=10, decimal_places=2,default=0,verbose_name= 'Salary amount Total')
+    totalamount =  models.DecimalField(max_digits=10, decimal_places=2,default=0,verbose_name= 'Salary amount Total')
     entity = models.ForeignKey(entity,on_delete=models.CASCADE,verbose_name= 'entity',null= True)
     createdby = models.ForeignKey(to= User, on_delete= models.CASCADE,null=True)
 
