c9306e6ca0111cb6b09c7e9ecad3b0a41c95a0e4
sushil0586 <85013199+sushil0586@users.noreply.github.com>
2025-02-22 08:33:16 +0530

    Update settings.py

--- FA/settings.py
+++ FA/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-(zyb)qx!o_p@$vjqscb=p+)8&-(tj(v*ne_=qc(r@7f(%%a5ey
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['3.86.224.93']
 
 AUTH_USER_MODEL = "Authentication.User"
 # Application definition
