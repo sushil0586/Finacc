8e58c015333dfe68a79a0f49cbb469ab41a5bb4b
sushil0586 <85013199+sushil0586@users.noreply.github.com>
2025-02-23 18:38:53 +0530

    Update urls.py

--- FA/urls.py
+++ FA/urls.py
@@ -17,6 +17,7 @@ from django.urls import path,include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from django.http import JsonResponse
 schema_view = get_schema_view(
    openapi.Info(
       title="Snippets API",
