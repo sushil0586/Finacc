c020cf5164ca4cb186b51a22c59ff015d2666f34
sushil0586 <85013199+sushil0586@users.noreply.github.com>
2025-03-01 10:18:00 +0530

    Update settings.py

--- FA/settings.py
+++ FA/settings.py
@@ -174,8 +174,9 @@ USE_L10N = True
 USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'  # URL to access static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Collect all static files here
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Where Django looks for static files
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
