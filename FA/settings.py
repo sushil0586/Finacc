215ec5674d5623d7bf638bdf191c509e367bfac0
sushil0586 <85013199+sushil0586@users.noreply.github.com>
2025-04-29 16:24:20 +0530

    Update settings.py

--- FA/settings.py
+++ FA/settings.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from pathlib import Path
 import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
