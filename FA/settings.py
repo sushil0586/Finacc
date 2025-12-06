a6317c57b80400f74a97f12f0da406c86039e68e
sushil0586 <85013199+sushil0586@users.noreply.github.com>
2025-12-06 14:54:02 +0530

    Update settings.py

--- FA/settings.py
+++ FA/settings.py
@@ -13,6 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 LOG_DIR = os.path.join(BASE_DIR, 'logs')
 os.makedirs(LOG_DIR, exist_ok=True)  # <== this MUST be above the LOGGING config
 
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
 # Quick-start development settings - unsuitable for production
@@ -227,6 +229,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
